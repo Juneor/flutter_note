c11e55973fe40ebe399bcfb281856d783576a713
Jeek <weimeiapp@gmail.com>
2019-08-27 20:04:25 +0800

    1、SnackBar使用

--- lib/ui/widgets/DrawerLayout.dart
+++ lib/ui/widgets/DrawerLayout.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_note/ui/pages/PlayerPage.dart';
@@ -18,77 +20,107 @@ class DrawerLayout extends StatelessWidget {
           padding: EdgeInsets.all(0),
           physics: BouncingScrollPhysics(),
           children: <Widget>[
-            UserAccountsDrawerHeader(
-              accountName: Text("\${user.name}"),
-              accountEmail: Text("\${user.email}"),
-              currentAccountPicture: CircleAvatar(
-                backgroundImage: CachedNetworkImageProvider(
-                    "http://gionchat.com/static/upload/2019/06/26/719b4cabe5f85ac3ee146ce849f45591.jpg"),
-              ),
-              decoration: BoxDecoration(
-                  image: DecorationImage(
-                fit: BoxFit.cover,
-                image: CachedNetworkImageProvider(
-                    "http://gionchat.com/static/upload/2019/06/26/719b4cabe5f85ac3ee146ce849f45591.jpg"),
-              )),
+            Stack(
+              children: <Widget>[
+                UserAccountsDrawerHeader(
+                  accountName: Text("\${user.name}"),
+                  accountEmail: Text("\${user.email}"),
+                  currentAccountPicture: CircleAvatar(
+                    backgroundImage: CachedNetworkImageProvider(
+                        "http://gionchat.com/static/upload/2019/06/26/719b4cabe5f85ac3ee146ce849f45591.jpg"),
+                  ),
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: CachedNetworkImageProvider(
+                        "http://gionchat.com/static/upload/2019/06/26/719b4cabe5f85ac3ee146ce849f45591.jpg"),
+                  )),
+                ),
+//                BackdropFilter(filter: ImageFilter.blur(
+//                  sigmaX: 10,
+//                  sigmaY: 10,
+//                ),
+//                  child: Container(
+//                    color: Colors.white10,
+//                    padding: EdgeInsets.all(null),
+//                    child: null,
+//                  ),
+//                )
+              ],
             ),
             InkWell(
                 child: ListTile(
-              title: Text("用户反馈"),
-              leading: Icon(Icons.feedback,color: Color(0xFF3FEECA),),
+              title: Text(
+                "用户反馈",
+              ),
+              leading: Icon(
+                Icons.feedback,
+                color: Color(0xFFf86734),
+              ),
               onTap: () {
                 print("用户反馈");
               },
-                  dense: true,
+              dense: true,
             )),
             Divider(
               height: 0.1,
             ),
             ListTile(
               title: Text("系统设置"),
-              leading: Icon(Icons.settings,color: Color(0xFF0AA848),),
+              leading: Icon(
+                Icons.settings,
+                color: Color(0xFF0AA848),
+              ),
             ),
             Divider(
               height: 0.1,
             ),
             ListTile(
               title: Text("我要发布"),
-              leading: Icon(Icons.send,color: Color(0xFF54C5F8),),
+              leading: Icon(
+                Icons.send,
+                color: Color(0xFF54C5F8),
+              ),
             ),
             Divider(
               height: 0.1,
             ),
             ListTile(
               title: Text("Android iOS图标制作"),
-              leading: Icon(Icons.android,color: Color(0xFF3DDB84),),
-            ),
-            Divider(
-              height: 0.1,
-            ),
-
-            ListTile(
-              title: Text("Android iOS启动图制作"),
-              leading: Icon(Icons.phone_iphone,color: Color(0xFFF5D414),),
+              leading: Icon(
+                Icons.android,
+                color: Color(0xFF3DDB84),
+              ),
+              onTap: () {
+                Navigator.pop(context);
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('敬请期待!')));
+              },
             ),
             Divider(
               height: 0.1,
             ),
             ListTile(
               title: Text("注销"),
-              leading: Icon(Icons.exit_to_app,color: Color(0xFFEE0000),),
+              leading: Icon(
+                Icons.exit_to_app,
+                color: Color(0xFFEE0000),
+              ),
             ),
             Divider(
               height: 0.1,
             ),
             ListTile(
               title: Text("演员表"),
-              leading: Icon(Icons.local_movies,color: Color(0xFF303030),),
-              onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context){
-
+              leading: Icon(
+                Icons.local_movies,
+                color: Color(0xFF303030),
+              ),
+              onTap: () =>
+                  Navigator.push(context, MaterialPageRoute(builder: (context) {
                 return PlayerPage();
               })),
             ),
-           
           ],
         ),
       ),
